facba737ceeef1a183c08f8f4be6b08817823d2e
Christopher S <Christopher S>
2016-03-01 13:31:01 -0700

    working with new php callback

--- Assignment03/www/getLists.php
+++ Assignment03/www/getLists.php
@@ -1,4 +1,4 @@
-<?php
+<?php header('content-type: application/json; charset=utf-8');
     $UserId = -1;
     $UserId = $_REQUEST["UserId"];
     //echo "test1";
@@ -27,6 +27,6 @@
     else{
         echo "ERROR! No Lists found";
     }
-
+    $data = array(1, 2, 3, 4, 5, 6, 7, 8, 9);
     echo $_GET['callback'].'('.json_encode($data).')';
 ?>
