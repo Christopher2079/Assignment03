fdc9dc37a0e6c8d58172af0bd39d92fb874143f8
Christopher S <Christopher S>
2016-03-25 15:11:36 -0600

    Massive changes to how the php works, all php code can now be found in the assignment3.php file and all php will now be called from this file.

--- Assignment03/www/js/assignment3.js
+++ Assignment03/www/js/assignment3.js
@@ -1,31 +1,202 @@
 var userIsLoggedIn = false;
 var loggedInUser = -1;
 
+function DisplayInfo() {
+    "use strict";
+    window.setInterval(function () {
+        getList();   
+    }, 500);
+}
 
 function getList() {
-    "use strict";
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function () {
-        if (xhttp.readyState === 4 && xhttp.status === 200) {
-            //$('#ListDiv').remove();
-            $('#UserLists').html(xhttp.responseText).enhanceWithin();
-            
-            
+   "use strict";
+    $.ajax({ url: 'http://icarus.cs.weber.edu/~cs79098/CS3750/Assign3/assignment3.php',
+        data: {action: 'getLists', UserId: loggedInUser},
+        type: 'post',
+        success: function(output) {
+           // alert(output);
+            $('#UserLists').html(output).enhanceWithin();
         }
-    };
-    xhttp.open("GET", "http://icarus.cs.weber.edu/~cs79098/CS3750/Assign3/getLists.php?UserId=" + loggedInUser, true);
-    xhttp.send();
+    });  
         
 
 }
 
-function DisplayInfo() {
+function signIn() {
     "use strict";
-    window.setInterval(function () {
-       getList(); 
-    }, 500);
+    var userName = document.getElementById("userName").value,
+        password = document.getElementById("password").value,
 
+    //get the hash value of the password
+        hashPassword = sha256(password);
+    //call the server and verify & get their todo list
+    if (userName === "") {
+        document.getElementById("ableTologin").innerHTML = "please enter a username";
+    } else if (password === "") {
+        document.getElementById("ableTologin").innerHTML = "please enter a Password";
+    } else {
+        document.getElementById("ableTologin").innerHTML = "";
 
+        $.ajax({ url: 'http://icarus.cs.weber.edu/~cs79098/CS3750/Assign3/assignment3.php',
+            data: {action: 'signIn', userName: "'" + userName + "'", hashPassword: "'" + hashPassword + "'"},
+            type: 'post',
+            success: function(output) {
+                loggedInUser = output;
+                if (output > 0) {
+                    document.getElementById("ableTologin").innerHTML = "User has been Logged in";
+                    userIsLoggedIn = true;
+                    loggedInUser = output;
+
+                } else {
+                    document.getElementById("ableTologin").innerHTML = output;
+                }
+            }
+        }); 
+    }
+}
+
+function createUser() {
+    "use strict";
+    var userName = "",
+        password = document.getElementById("pass").value,
+        cPass = document.getElementById("confirmPass").value;
+    userName = document.getElementById("user").value;
+
+    // check that the passwords match
+    if (password !== cPass || password === "" || cPass === "") {
+        document.getElementById("createdUser").innerHTML = "Password did not match. Try again.";
+
+    } else if (password.length < 6) {
+        document.getElementById("createdUser").innerHTML = "Password Must be greater than 5 characters";
+
+    } else if (typeof userName === 'undefined' || userName === "") {
+        document.getElementById("createdUser").innerHTML = "Please enter in a user name";
+    } else {
+        // convert the password to a hashPassword
+        var hashPassword = sha256(password),
+            hashC = sha256(cPass);
+
+        if (hashPassword !== hashC) {
+            document.getElementById("createdUser").innerHTML = "hashPassword didn't match";
+
+        } else {
+
+            $.ajax({ url: 'http://icarus.cs.weber.edu/~cs79098/CS3750/Assign3/assignment3.php',
+                data: {action: 'createUser', userName: "'" + userName + "'", hashPassword: "'" + hashPassword + "'"},
+                type: 'post',
+                success: function(output) {
+                    // alert(output);
+                    document.getElementById("createdUser").innerHTML = output;
+                }
+            }); 
+            
+            
+        }
+    }
+}
+
+function addItem(ListId) {
+    var ItemName = document.getElementById("addItemName"+ListId).value;
+
+    if(ItemName === "") {
+        alert("Item Not Added, Please enter a item name");
+    } else {
+        $.ajax({ url: 'http://icarus.cs.weber.edu/~cs79098/CS3750/Assign3/assignment3.php',
+            data: {action: 'addItem', ItemName: "'" + ItemName + "'", ListId: ListId},
+            type: 'post',
+            success: function(output) {
+            }
+        });  
+    }
+    document.getElementById("addItemName").value = "";
+}
+
+function removeItem(ItemId) {    
+    $.ajax({ url: 'http://icarus.cs.weber.edu/~cs79098/CS3750/Assign3/assignment3.php',
+        data: {action: 'removeItem', ItemId: ItemId},
+        type: 'post',
+        success: function(output) {
+           // alert(output);
+        }
+    }); 
+}
+
+function checkOffItem(ItemId, isCheckedOff) {
+    var checkOff = -1;
+    if(!isCheckedOff) {
+        checkOff = 1;
+    } else {
+        checkOff = 0;
+    }
+    
+    $.ajax({ url: 'http://icarus.cs.weber.edu/~cs79098/CS3750/Assign3/assignment3.php',
+        data: {action: 'checkOffItem', ItemId: ItemId, checkOff: checkOff},
+        type: 'post',
+        success: function(output) {
+           // alert(output);
+        }
+    }); 
+}
+
+function createList(){
+    var ListName = document.getElementById("list-name").value;
+
+    if(ListName === "") {
+        alert("List Not Added, Please enter a List name");
+    } else {
+        //call the php code to add the list to the list table
+        //also have to link it to the user with the userlist table
+        $.ajax({ url: 'http://icarus.cs.weber.edu/~cs79098/CS3750/Assign3/assignment3.php',
+            data: {action: 'createList', UserId: "'" + loggedInUser + "'", ListName: "'" + ListName + "'"},
+            type: 'post',
+            success: function(output) {
+               alert(output);
+            }
+        }); 
+        
+        
+        /*
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function () {
+            //alert("Ready State: " + xhttp.readyState + ", Status: " + xhttp.status);
+            if (xhttp.readyState === 4 && xhttp.status === 200) {
+                document.getElementById("simpleReturn").innerHTML = xhttp.responseText;
+                //alert(xhttp.responseText);
+            }
+
+        };
+        //need to pass in ListName and listId
+        xhttp.open("GET", "http://icarus.cs.weber.edu/~cs79098/CS3750/Assign3/someLink.php", true);
+        xhttp.send();
+
+        */
+        
+        //This next call will be used to link the user to the new list in the UserList table
+        /*
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function () {
+            //alert("Ready State: " + xhttp.readyState + ", Status: " + xhttp.status);
+            if (xhttp.readyState === 4 && xhttp.status === 200) {
+                document.getElementById("simpleReturn").innerHTML = xhttp.responseText;
+                //alert(xhttp.responseText);
+            }
+
+        };
+        //need to pass in Logged in user ID, the new list ID will be grabbed by an sql statment for the "newest" list id
+        xhttp.open("GET", "http://icarus.cs.weber.edu/~cs79098/CS3750/Assign3/someLink.php", true);
+        xhttp.send();
+
+        */
+        
+    }
+    document.getElementById("list-name").value = "";
+    getList();
+}
+
+function removeList(ListId) {
+    //loggedInUser --> this is the active logged in user
+    //need to remove list, list linked to user, and all items that belog to that list.
+    
 }
 
 function createAddListPopup() {
@@ -94,179 +265,5 @@ function createEditItemPopup(ItemId, isCheckedOff) {
     //document.getElementById("ListPopups").innerHTML = PopupDiv;
     $('#ListPopups').html(PopupDiv).enhanceWithin();
 
-    
-}
-
-function signIn() {
-    "use strict";
-    var userName = document.getElementById("userName").value,
-        password = document.getElementById("password").value,
-
-    //get the hash value of the password
-        hashPassword = sha256(password);
-    //call the server and verify & get their todo list
-    if (userName === "") {
-        document.getElementById("ableTologin").innerHTML = "please enter a username";
-    } else if (password === "") {
-        document.getElementById("ableTologin").innerHTML = "please enter a Password";
-    } else {
-        document.getElementById("ableTologin").innerHTML = "";
-
-        var xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function () {
-            if (xhttp.readyState === 4 && xhttp.status === 200) {
-                loggedInUser = xhttp.responseText;
-                if (loggedInUser > 0) {
-                    document.getElementById("ableTologin").innerHTML = "User has been Logged in";
-                    userIsLoggedIn = true;
-
-                } else {
-                    document.getElementById("ableTologin").innerHTML = "Error, Incorrect Username or Password";
-
-                }
-            }
-        };
-        xhttp.open("GET", "http://icarus.cs.weber.edu/~cs79098/CS3750/Assign3/userLogin.php?UserName='" + userName + "'&Password='" + hashPassword + "'", true);
-        xhttp.send();
-    }
-}
-
-function createUser() {
-    "use strict";
-    var userName = "",
-        password = document.getElementById("pass").value,
-        cPass = document.getElementById("confirmPass").value;
-    userName = document.getElementById("user").value;
-
-    // check that the passwords match
-    if (password !== cPass || password === "" || cPass === "") {
-        document.getElementById("createdUser").innerHTML = "Password did not match. Try again.";
-
-    } else if (password.length < 6) {
-        document.getElementById("createdUser").innerHTML = "Password Must be greater than 5 characters";
-
-    } else if (typeof userName === 'undefined' || userName === "") {
-        document.getElementById("createdUser").innerHTML = "Please enter in a user name";
-    } else {
-        // convert the password to a hash
-        var hash = sha256(password),
-            hashC = sha256(cPass);
-
-        if (hash !== hashC) {
-            document.getElementById("createdUser").innerHTML = "hash didn't match";
-
-        } else {
-            var xhttp = new XMLHttpRequest();
-            xhttp.onreadystatechange = function () {
-                if (xhttp.readyState === 4 && xhttp.status === 200) {
-                    document.getElementById("createdUser").innerHTML = xhttp.responseText;
-                }
-            };
-            xhttp.open("GET", "http://icarus.cs.weber.edu/~cs79098/CS3750/Assign3/chkUserExists.php?UserName='" + userName + "'&Password='" + hash + "'", true);
-            xhttp.send();
-
-        }
-    }
-}
-
-
-
-function addItem(ListId) {
-    var ItemName = document.getElementById("addItemName"+ListId).value;
-
-    if(ItemName === "") {
-        alert("Item Not Added, Please enter a item name");
-    } else {        
-        
-        var xhttp = new XMLHttpRequest();
-        //need to pass in the item name and the list id
-        xhttp.open("GET", "http://icarus.cs.weber.edu/~cs79098/CS3750/Assign3/addItem.php?ItemName='" + ItemName + "'&ListId=" + ListId , true);
-        xhttp.send();
-    }
-    document.getElementById("addItemName").value = "";
-}
-
-function removeItem(ItemId) {    
-    var xhttp = new XMLHttpRequest();
-    //need to pass in the item Id
-    xhttp.open("GET", "http://icarus.cs.weber.edu/~cs79098/CS3750/Assign3/removeItem.php?ItemId=" + ItemId, true);
-    xhttp.send();
-}
-
-function checkOffItem(ItemId, isCheckedOff) {
-    var xhttp = new XMLHttpRequest();
-    if(!isCheckedOff) {
-        //alert("Item has been checked off");
-        //Need to pass a value = 1 and that will be used to change the status of the item
-        xhttp.open("GET", "http://icarus.cs.weber.edu/~cs79098/CS3750/Assign3/isCheckedOff.php?ItemId=" + ItemId + "&isCheckedOff=1", true);
-        xhttp.send();
-    } else {
-        //alert("Item has been unchecked");
-        //Need to pass a value = 0 and that will be used to change the status of the item
-        xhttp.open("GET", "http://icarus.cs.weber.edu/~cs79098/CS3750/Assign3/isCheckedOff.php?ItemId=" + ItemId + "&isCheckedOff=0", true);
-        xhttp.send();
-        
-    }
-}
-
-function createList(){
-    var ListName = document.getElementById("list-name").value;
-
-    if(ListName === "") {
-        alert("List Not Added, Please enter a List name");
-    } else {
-        //call the php code to add the list to the list table
-        //also have to link it to the user with the userlist table
-        var xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function(){
-            if(xhttp.readyState === 4 && xhttp.status === 200){
-                document.getElementById("listCreated").innerHTML = xhttp.responseText;
-            }
-        };
-        xhttp.open("GET","http://icarus.cs.weber.edu/~cs79098/CS3750/Assign3/listCreate.php?UserID='" + loggedInUser + "'&ListName='" + ListName + "'", true);
-        xhttp.send();
-        
-        
-        /*
-        var xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function () {
-            //alert("Ready State: " + xhttp.readyState + ", Status: " + xhttp.status);
-            if (xhttp.readyState === 4 && xhttp.status === 200) {
-                document.getElementById("simpleReturn").innerHTML = xhttp.responseText;
-                //alert(xhttp.responseText);
-            }
-
-        };
-        //need to pass in ListName and listId
-        xhttp.open("GET", "http://icarus.cs.weber.edu/~cs79098/CS3750/Assign3/someLink.php", true);
-        xhttp.send();
-
-        */
-        
-        //This next call will be used to link the user to the new list in the UserList table
-        /*
-        var xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function () {
-            //alert("Ready State: " + xhttp.readyState + ", Status: " + xhttp.status);
-            if (xhttp.readyState === 4 && xhttp.status === 200) {
-                document.getElementById("simpleReturn").innerHTML = xhttp.responseText;
-                //alert(xhttp.responseText);
-            }
-
-        };
-        //need to pass in Logged in user ID, the new list ID will be grabbed by an sql statment for the "newest" list id
-        xhttp.open("GET", "http://icarus.cs.weber.edu/~cs79098/CS3750/Assign3/someLink.php", true);
-        xhttp.send();
-
-        */
-        
-    }
-    document.getElementById("list-name").value = "";
-    getList();
-}
-
-function removeList(ListId) {
-    //loggedInUser --> this is the active logged in user
-    //need to remove list, list linked to user, and all items that belog to that list.
     
 }
---
+++ Assignment03/www/phpcode/assignment3.php
@@ -0,0 +1,248 @@
+<?php header('Access-Control-Allow-Origin: *');
+
+    if(isset($_POST['action']) && !empty($_POST['action'])) {
+        $action = $_POST['action'];
+        switch($action) {
+            case 'getLists'     : getLists();break;
+            case 'signIn'       : signIn();break;
+            case 'createUser'   : createUser();break;
+            case 'addItem'      : addItem();break;
+            case 'removeItem'   : removeItem();break;
+            case 'checkOffItem' : checkOffItem();break;
+            case 'createList'   : createList();break;
+            case 'removeList'   : removeList();break;
+            default : echo "alert('Wha? How did you get here???')";
+        }
+    }
+
+    function getLists(){
+        if(isset($_POST['UserId']) && !empty($_POST['UserId'])) {
+            $UserId = $_POST['UserId'];
+            if($UserId > 0) {
+                //echo "Test2";
+                $db = mysqli_connect('localhost',
+                                    'W01162084',
+                                    'Jaysoncs!',
+                                    'W01162084');
+
+                $sql = "SELECT li.ListId, li.ListName FROM Lists li 
+                        Join UserList ul on li.ListId = ul.ListId
+                        JOIN User us ON ul.UserId = us.UserId
+                        WHERE us.UserId = " . $UserId;
+
+                echo    '<div id="ListDiv">
+
+                        <ul data-role="listview" data-icon="false" data-theme="a" data-split-theme="b" data-inset="true">
+                            <li><a href="#AddList" data-rel="popup" data-position-to="window" data-transition="pop" onclick="createAddListPopup();">
+                                <h2><Center>Add New List</Center></h2></a>
+                            </li>
+                        </ul>';
+
+                $result = mysqli_query($db, $sql);
+                    if ($result) {
+                        while ($row = mysqli_fetch_assoc($result)) {
+                            echo    '<ul data-role="listview" data-split-icon="plus" data-theme="a" data-split-theme="b" data-inset="true">
+                                    <li><a href="#" >
+                                    <h2>'.htmlspecialchars($row['ListName']).'</h2>
+                                    </a><a href="#'.htmlspecialchars($row['ListId']).'_addItem" data-rel="popup" data-position-to="window" data-transition="pop" onclick="createAddItemPopup('.htmlspecialchars($row['ListId']).');">Add Item</a>
+                                    </li>';
+
+
+
+                            $sql = "Select ItemId, ItemName, ItemCheckedOff  FROM ListItems Where ListId = ".htmlspecialchars($row['ListId']);
+                            $itemResult = mysqli_query($db, $sql);
+                            if($itemResult) {
+                                while($row = mysqli_fetch_assoc($itemResult)) {
+                                    echo '<li data-icon="info"><a href="#">';
+
+                                    //Check to see if the item in the list has been check off
+                                    if(!htmlspecialchars($row['ItemCheckedOff'])){
+                                        echo'<h2>'.htmlspecialchars($row['ItemName']).'</h2>';
+                                    }
+                                    else {
+                                        echo '<h2><strike>'.htmlspecialchars($row['ItemName']).'</strike></h2>';
+                                    }
+
+                                    echo '</a><a href="#'.htmlspecialchars($row['ItemId']).'_editItem" data-theme="a" data-rel="popup" data-position-to="window" data-transition="pop" onclick="createEditItemPopup('.htmlspecialchars($row['ItemId']).', '.htmlspecialchars($row['ItemCheckedOff']).' );">'.htmlspecialchars($row['ItemName']).'</a>
+                                        </li>';
+
+                                }
+                            }
+                        echo '</ul>';
+                        echo '</div>';
+
+                    }
+                } 
+                else {
+                    echo "No Lists Found";
+                }
+
+            } 
+            else{
+            echo "Please Login to see your lists";
+            }
+        }
+    }
+
+    function signIn(){
+        $userName = $_POST["userName"];
+        $hashPassword = $_POST["hashPassword"];
+
+        if($userName != "") {
+            //echo "Test2";
+            $db = mysqli_connect('localhost',
+                                'W01162084',
+                                'Jaysoncs!',
+                                'W01162084');
+
+            $sql = "Select Password, UserId From User Where UserName = " . $userName;
+            $result = mysqli_query($db, $sql);
+            if (mysqli_num_rows($result) > 0) {
+                while ($row = mysqli_fetch_assoc($result)) {
+                    if("'".htmlspecialchars($row['Password'])."'" == $hashPassword) {
+                        echo htmlspecialchars($row['UserId']);
+                    }else{
+                        echo "Password Incorrect";
+                    }
+                }
+            } 
+            else {
+                echo "User Does not exist";
+            }
+        } 
+        else{
+            echo "Error, bad username";
+        }
+    }
+
+    function createUser() {
+        
+        $UserName = $_POST["userName"];
+        $Password = $_POST["hashPassword"];
+
+        if($UserName != "") {
+            //echo "Test2";
+            $db = mysqli_connect('localhost',
+                                'W01162084',
+                                'Jaysoncs!',
+                                'W01162084');
+
+            $sql = "Select userName From User Where UserName = " . $UserName;
+
+            $result = mysqli_query($db, $sql);
+            if (mysqli_num_rows($result) > 0) {
+                while ($row = mysqli_fetch_assoc($result)) {
+                    echo "User Already Exists";
+                }
+            } 
+            else {
+                $sql = "Insert Into User Values(null,".$UserName . "," . $Password . ")";
+                mysqli_query($db, $sql);
+                echo "User created. Go ahead and login";
+
+            }
+        } 
+        else{
+            echo "ERROR! Bad UserName";
+        }
+    }
+
+    function addItem() {
+        $ItemName = $_POST["ItemName"];
+        $ListId = $_POST["ListId"];
+
+        if($ItemName != "" || $ListId < 0) {
+            //echo "Test2";
+            $db = mysqli_connect('localhost',
+                                'W01162084',
+                                'Jaysoncs!',
+                                'W01162084');
+
+            $sql = "Insert Into ListItems Values(null, ".$ItemName.", NOW(), ".$ListId.", 0)";
+            mysqli_query($db, $sql);
+            echo "Item Name: $ItemName | ListId: $ListId" ;
+        } 
+        else{
+            echo "ERROR! Item Name Wrong or List Id < 0";
+        }
+    }
+
+    function removeItem() {
+        $ItemId = $_POST["ItemId"];
+
+        if($ItemId > 0) {
+            $db = mysqli_connect('localhost',
+                                'W01162084',
+                                'Jaysoncs!',
+                                'W01162084');
+
+            $sql = "DELETE FROM ListItems WHERE ItemId = $ItemId";
+
+
+            mysqli_query($db, $sql);
+
+        } 
+        else{
+            echo "ERROR! ItemId < 0";
+        }
+        
+    }
+
+    function checkOffItem() {
+        
+        $ItemId = $_POST["ItemId"];
+        $isCheckedOff = $_POST["checkOff"];
+
+        if($ItemId > 0 || $isCheckedOff > 0) {
+            //echo "Test2";
+            $db = mysqli_connect('localhost',
+                'W01162084',
+                'Jaysoncs!',
+                'W01162084');
+
+            $sql = "UPDATE ListItems SET ItemCheckedOff =".$isCheckedOff." WHERE ItemId = ".$ItemId."";
+            mysqli_query($db, $sql);
+        } else {
+            echo "ERROR! ItemId < 0 || ischeckedoff < 0";
+        }
+    }
+
+    function createList() {
+        $UserID = $_POST["UserId"];
+        $ListName = $_POST["ListName"];
+
+        $db = mysqli_connect('localhost',
+                            'W01162084',
+                            'Jaysoncs!',
+                            'W01162084');
+        //check to see if list name already exists and get the listID
+        $sql = "SELECT ListID FROM Lists WHERE ListName = $ListName";
+
+        $result = mysqli_query($db, $sql);
+        if (mysqli_num_rows($result) > 0){
+            while ($row = mysqli_fetch_row($result)){
+                $ListID = $row[0];
+            }
+        }
+        else{
+            //if it doesn't exist create the list and get the listID
+            $sql2 = "INSERT INTO Lists Values(null, $ListName)";
+            mysqli_query($db, $sql2);
+
+            $result = mysqli_query($db, $sql);
+            if (mysqli_num_rows($result) > 0){
+                while ($row = mysqli_fetch_row($result)){
+                    $ListID = $row[0];
+                }
+            }
+        }
+        //connect the list to the user
+        $sql = "INSERT INTO UserList Values(null, $UserID , $ListID )";
+        mysqli_query($db, $sql);
+    }
+
+    function removeList() {
+        //remove the list here
+    }
+
+?>
